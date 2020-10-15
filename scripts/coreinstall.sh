ce46bf2078bb9f11c6136094e8a15b32e10850df
Yannick Buntsma <yannickbuntsma@users.noreply.github.com>
2020-10-15 14:23:09 +0200

    Update coreinstall.sh

--- scripts/coreinstall.sh
+++ scripts/coreinstall.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
+# No idea what this does, so skipping it for now.
 # install drivers
-brew tap caskroom/drivers
+# brew tap caskroom/drivers
 
 # install tools
 brew cask install appcleaner
 brew cask install bettertouchtool
 brew cask install boom
+brew cask install fantastical
 brew cask install figma
 brew cask install firefox
+brew cask install font-victor-mono
 brew cask install fork
 brew cask install google-chrome
-# brew cask install iterm2
+brew cask install iglance
+brew cask install iterm2
 brew cask install paste
-brew cask install sip
+brew cask install qmk-toolbox
+brew cask install react-native-debugger
+brew cask install rectangle
 brew cask install slack
-brew cask install spectacle
+brew cask install smcfancontrol
 brew cask install spotify
+brew cask install visual-studio-code-insiders
+brew cask install vlc
 brew cask install whatsapp
